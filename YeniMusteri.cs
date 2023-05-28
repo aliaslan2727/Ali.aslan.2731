bb5ada83a1dce1286733538b14d59ec4185a80d7
aliaslan2727 <115794480+aliaslan2727@users.noreply.github.com>
2023-05-28 14:15:14 +0300

    Add files via upload
    ücret oda numarası üzerinden değişiklik yapıldı.

--- YeniMusteri.cs
+++ YeniMusteri.cs
@@ -27,12 +27,76 @@ namespace WindowsFormsApp1
 
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void BtnOda102_Click(object sender, EventArgs e)
+        {
+            txtOdaNo.Text = "102";
+        }
+
+        private void YeniMusteri_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void YeniMusteri_Load(object sender, EventArgs e)
+        private void BtnBosOda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yeşil Renkli Odalar Boş Odaları Gösterir.");
+        }
+
+        private void BtnOda101_Click(object sender, EventArgs e)
+        {
+            txtOdaNo.Text = "101";
+        }
+
+        private void BtnOda103_Click(object sender, EventArgs e)
+        {
+            txtOdaNo.Text = "103";
+        }
+
+        private void BtnODa104_Click(object sender, EventArgs e)
+        {
+            txtOdaNo.Text = "104";
+        }
+
+        private void BtnOda105_Click(object sender, EventArgs e)
+        {
+            txtOdaNo.Text = "105";        }
+
+        private void BtnOda106_Click(object sender, EventArgs e)
+        {
+            txtOdaNo.Text = "106";
+        }
+
+        private void BtnOda107_Click(object sender, EventArgs e)
+        {
+            txtOdaNo.Text = "107";
+        }
+
+        private void BtnOda108_Click(object sender, EventArgs e)
+        {
+            txtOdaNo.Text = "108";
+        }
+
+        private void BtnOda109_Click(object sender, EventArgs e)
+        {
+            txtOdaNo.Text = "109";
+        }
+
+        private void BtnDoluOda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kırmızı Renkli Butonlar Dolu Odaları Gösterir.");
+        }
+
+        private void txtCinsiyet_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
         {
 
         }
